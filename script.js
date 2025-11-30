077206f69777d5dd90060d9d642ac3620a95aaf7
Ritik Sharma <ritiksharma10508@gmail.com>
2025-11-30 23:09:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function days(input) {
+	if(input%1000===0){
+	console.log("366")	
+	}else if (input%100===0) {
+		console.log("365")
+	} else if (input%4===0) {
+		console.log("364")
+	} else {
+		console.log("365")
+	} 
+	
+}
+return days;
+document.getElementById("output").innerHTML = days();
+
+
