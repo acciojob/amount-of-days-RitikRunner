7c04e9eb8d087bb87e870ee96b3f0c0d071ccec4
Ritik Sharma <ritiksharma10508@gmail.com>
2025-12-12 23:56:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 //your JS code here. If required.
-function days(input) {
-	if(input%1000===0){
-	console.log("366")	
-	}else if (input%100===0) {
-		console.log("365")
-	} else if (input%4===0) {
-		console.log("364")
-	} else {
-		console.log("365")
-	} 
+function daysOfAYear(year) {
+	return isLeapYear(year)? 366 : 365
 	
 }
-return days;
-document.getElementById("output").innerHTML = days();
+funtion isLeapYear(year){
+	return year%400===0 || (year%100!==0 && year%4===0)  
+}
+const year = prompt("Enter a year.")
+alert(daysOfAYear(year))
 
 
