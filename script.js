1c514e234ca93141ec903947e2c46fa654db436b
Ritik Sharma <ritiksharma10508@gmail.com>
2025-12-12 23:59:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 //your JS code here. If required.
 function daysOfAYear(year) {
-	return isLeapYear(year)? 366 : 365
+	return isLeapYear(year)? 366 : 365;
 	
 }
-funtion isLeapYear(year){
-	return year%400===0 || (year%100!==0 && year%4===0)  
+function isLeapYear(year){
+	return year%400===0 || (year%100!==0 && year%4===0); 
 }
-const year = prompt("Enter a year.")
-alert(daysOfAYear(year))
+ 
+const year = prompt("Enter a year.");
+alert(daysOfAYear(year));
 
 
